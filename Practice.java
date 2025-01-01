156d76b7f4068b5350139a9585094c6c359ceda4
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-02 00:55:32 +0530

    practice questions

--- Practice.java
+++ Practice.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// This file illustrates the list of practice questions for the java.
 public class Practice {
     public static void main(String[] args) {
         // 1. Even or Odd
@@ -11,7 +12,45 @@ public class Practice {
         } else {
             System.out.println("Odd");
         }
-        input.close();
 
+        // 2. Valid/Invalid Triangle.
+        System.out.println("Enter first angle");
+        int angle1 = input.nextInt();
+        System.out.println("Enter second angle");
+        int angle2 = input.nextInt();
+        System.out.println("Enter third angle");
+        int angle3 = input.nextInt();
+        if (angle1 + angle2 + angle3 == 180 && ( angle1 > 0 && angle2 > 0 && angle3> 0)) {
+            System.out.println("Valid Triangle");
+        } else {
+            System.out.println("Invalid Triangle");
+        }
+
+
+        // 3. Leap Year
+        System.out.println("Enter the year.");
+        int year = input.nextInt();
+        if ( year % 100 == 0){
+            if ( year % 400 == 0 ){
+                System.out.println("Leap Year");
+            } else {
+                System.out.println("Not Leap Year");
+            }
+        } else {
+            if ( year % 4 == 0 ){
+                System.out.println("Leap Year");
+            } else {
+                 System.out.println("Not Leap Year");
+            }
+        }
+         
+
+
+        
+
+
+
+
+        input.close();
     }
 }
