37d027abf758217526e5e785158a6f3f9ba6baa4
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-10 21:49:35 +0530

    Functions

---
+++ Functions/Functions.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        welcomingGreeting();
+        System.out.println("Good Bye!");
+
+    }
+
+    public static void welcomingGreeting() {
+        System.out.println("Hi, Good Morning!");
+        System.out.println("Hope You are Having a Good Day!");
+    }
+}
