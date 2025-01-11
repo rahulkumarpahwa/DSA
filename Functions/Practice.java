a399568ab4c249d82a2de6f6a8746b460364aa11
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-11 21:34:29 +0530

    square and square roo of the numbers

--- Functions/Practice.java
+++ Functions/Practice.java
@@ -1,15 +1,22 @@
 package Functions;
 
+import java.lang.Math;
+
 public class Practice {
     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Square of the no. is " + getSquare(i));
+        }
+        System.out.println();
+
+        System.out.println("Square root of no. from 1 to 10 ");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Math.sqrt(i));
+        }
     }
 
-    public static void num(int x) {
-        if (x > 0) {
-            return;
-        }
-        num(x + 1);
-        x++;
+    public static int getSquare(int x) {
+        return x * x;
 
     }
 }
