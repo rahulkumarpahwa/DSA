e58e189c6b60c2f272be8cee3a5e2ea570c44401
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-12 22:21:49 +0530

    check armstrong and print armstrong between the range

--- Functions/Practice.java
+++ Functions/Practice.java
@@ -10,13 +10,25 @@ public class Practice {
         System.out.println();
 
         System.out.println("Square root of no. from 1 to 10 ");
-        for (int i = 4; i <= 10; i+=2) {
+        for (int i = 4; i <= 10; i += 2) {
             System.out.println(Math.sqrt(i));
         }
+
+        int one = 10, two = 20;
+        System.out.println("one :" + one + " two :" + two);
+        swap(one, two);
+        System.out.println("one :" + one + " two :" + two);
+
     }
 
     public static int getSquare(int x) {
         return x * x;
 
+    }
+
+    public static void swap(int one, int two) {
+        int temp = one;
+        one = two;
+        two = temp;
     }
 }
