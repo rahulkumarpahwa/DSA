69ea88a4e0716ab8d7c5c6ec1a002a4ac0e12025
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-15 14:17:00 +0530

    optimise running sum

--- ArrayList/RunningSum.java
+++ ArrayList/RunningSum.java
@@ -6,6 +6,10 @@ public class RunningSum {
     public static void main(String[] args) {
         int[] a = { 1, 2, 3, 4 };
         System.out.println(runningSum(a));
+        System.out.println();
+        for (int val : runningSumOptimize(a)) {
+            System.out.print(val + " ");
+        }
     }
 
     public static ArrayList<Integer> runningSum(int[] arr) { // Brute Force.
@@ -19,6 +23,15 @@ public class RunningSum {
             }
             result.add(sum);
 
+        }
+        return result;
+    }
+
+    public static int[] runningSumOptimize(int[] arr) { // optimize
+        int[] result = new int[arr.length];
+        result[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result[i] = result[i - 1] + arr[i];
         }
         return result;
     }
