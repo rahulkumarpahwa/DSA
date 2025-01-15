804ab2ad4392311f39054aabe50fd688c02666ae
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-15 22:24:24 +0530

    matrix multiplication

---
+++ Leetcode/MatrixMultiplication.java
@@ -0,0 +1,40 @@
+package Leetcode;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] b = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] c = new int[a.length][b[0].length];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                int sum = 0;
+                for (int k = 0; k < a[0].length; k++) {
+                    sum += a[i][k] * b[k][j];
+                }
+                c[i][j] = sum;
+            }
+        }
+
+        // print matrix: a
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        } // print matrix : b
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        } // print matrix : c (product)
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[0].length; j++) {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
