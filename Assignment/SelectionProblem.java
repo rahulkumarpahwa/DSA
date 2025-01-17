572aa4b8f0acb565aa59f68edde7031bdd86c845
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-17 22:42:29 +0530

    brute force solution

---
+++ Assignment/SelectionProblem.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class SelectionProblem {
+
+    public static void main(String[] args) {
+        int[] a = { 2, 2, 4, 6, 7, 8, 9 };
+        int pos = 4; // 4th largest element.
+        // find the kth largest element.
+        System.out.println(kthLargestElement(a, pos));
+
+    }
+
+    public static int kthLargestElement(int[] a, int kth) {
+        int val = -1;
+         
+
+        return val;
+    }
+}
---
+++ Leetcode/twoSum.java
@@ -0,0 +1,38 @@
+// problem no. : 167 leetcode
+
+package Leetcode;
+
+import java.util.ArrayList;
+
+import ArrayList.arraylist;
+
+public class twoSum {
+
+    public static void main(String[] args) {
+        int[] a = { 2, 7, 11, 15 };
+        int target = 18;
+        System.out.println(findTwoSum(a, target));
+        System.out.println(findTwoSumOptimize(a, target));
+    }
+
+    public static ArrayList<Integer> findTwoSum(int[] a, int target) { // brute force.
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            int j = i + 1;
+            while (j < a.length) {
+                if (a[i] + a[j] == target) {
+                    result.add(i + 1);
+                    result.add(j + 1);
+                }
+                j++;
+            }
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> findTwoSumOptimize(int[] a, int target) { // optimize
+        ArrayList<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
