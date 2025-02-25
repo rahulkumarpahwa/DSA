62b5e57fb7d763b0a707ca50545ac4adcb33a0a5
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-02-26 01:37:52 +0530

    practice questions

---
+++ BitManipulation/PracticeQuestions/UpperToLowerCase.java
@@ -0,0 +1,10 @@
+package BitManipulation.PracticeQuestions;
+
+public class UpperToLowerCase {
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print((char) (ch | ' ')); // space is 32 in ascii value. so basically the space is setting a 6th
+                                                 // bit from right in the upper case which makes it lower case.
+        }
+    }
+}
