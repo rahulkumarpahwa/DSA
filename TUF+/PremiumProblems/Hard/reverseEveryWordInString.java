222d7a3d881d2b2cb26f751c28f467f92121c1d0
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-10-25 22:43:20 +0530

    Add implementation for reversing every word in a string

---
+++ TUF+/PremiumProblems/Hard/reverseEveryWordInString.java
@@ -0,0 +1,13 @@
+// https://takeuforward.org/plus/dsa/problems/reverse-every-word-in-a-string
+
+// brute solution: 
+class Solution {
+    public String reverseWords(String s) {
+        String str[] = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length - 1; i >= 0; i--) {
+            sb.append(str[i]).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
