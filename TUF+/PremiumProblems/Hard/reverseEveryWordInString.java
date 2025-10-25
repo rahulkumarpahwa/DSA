37329d2f696d603880590cc3614dd0bd10b763e9
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-10-25 22:44:00 +0530

    Add comment to clarify space removal in reverseWords method

--- TUF+/PremiumProblems/Hard/reverseEveryWordInString.java
+++ TUF+/PremiumProblems/Hard/reverseEveryWordInString.java
@@ -3,7 +3,7 @@
 // brute solution: 
 class Solution {
     public String reverseWords(String s) {
-        String str[] = s.trim().split("\\s+");
+        String str[] = s.trim().split("\\s+"); // removing the multiple spaces at once.
         StringBuilder sb = new StringBuilder();
         for (int i = str.length - 1; i >= 0; i--) {
             sb.append(str[i]).append(" ");
