2c5fa7f3fea3fa0ba4df4f57027456c4a7079c12
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-10-25 23:04:50 +0530

    Add character-by-character implementation for reversing words in a string

--- TUF+/PremiumProblems/Hard/reverseEveryWordInString.java
+++ TUF+/PremiumProblems/Hard/reverseEveryWordInString.java
@@ -11,3 +11,28 @@ class Solution {
         return sb.toString().trim();
     }
 }
+
+// character by character: 
+class Solution {
+  public String reverseWords(String s) {
+    // by character by character:
+    List<String> res = new ArrayList<>();
+    int i = 0;
+    int start, end;
+    int n = s.length();
+    while (i < n) {
+      if (i > n) break;
+      while (i < n && s.charAt(i) == ' ') i++;
+      start = i;
+      while (i < n && s.charAt(i) != ' ') i++;
+      end = i - 1;
+      String eachWord = s.substring(start, end + 1);
+      res.add(eachWord);
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int j = res.size() - 1; j >= 0; j--) {
+      sb.append(res.get(j)).append(" ");
+    }
+    return sb.toString().trim();
+  }
+}
