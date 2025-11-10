03ab3e4a1add1eef7def5a03d018367ec5d2946a
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-10 22:41:57 +0530

    Add optimal implementation for finding the union of two sorted arrays

--- TUF+/Arrays/LogicBuilding/unionTwoSortedArrays.java
+++ TUF+/Arrays/LogicBuilding/unionTwoSortedArrays.java
@@ -19,3 +19,49 @@ class Solution {
         return arr;
     }
 }
+
+class Solution {
+  public int[] unionArray(int[] nums1, int[] nums2) {
+    // optimal:
+    List<Integer> res = new ArrayList<>();
+
+    int i = 0, j = nums1.length - 1, k = 0, l = nums2.length - 1;
+    while (i <= j && k <= l) {
+      if (nums1[i] == nums2[k]) {
+        if (!res.contains(nums1[i])) {
+          res.add(nums1[i]);
+        }
+        i++;
+        k++;
+      } else if (nums1[i] < nums2[k]) {
+        if (!res.contains(nums1[i])) {
+          res.add(nums1[i]);
+        }
+        i++;
+      } else {
+        if (!res.contains(nums2[k])) {
+          res.add(nums2[k]);
+        }
+        k++;
+      }
+    }
+    while (i <= j) {
+      if (!res.contains(nums1[i])) {
+        res.add(nums1[i]);
+      }
+      i++;
+    }
+    while (k <= l) {
+      if (!res.contains(nums2[k])) {
+        res.add(nums2[k]);
+      }
+      k++;
+    }
+    int arr[] = new int[res.size()];
+    int t = 0;
+    for (int num : res) {
+      arr[t++] = num;
+    }
+    return arr;
+  }
+}
