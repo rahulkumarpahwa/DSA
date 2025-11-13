157201b32a1f560eabe1b21f3d1f38db41c15b4e
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-13 22:32:10 +0530

    Add recursive implementation for binary search in a sorted array

--- TUF+/BinarySearch/Fundamentals/searchXinSortedArray.java
+++ TUF+/BinarySearch/Fundamentals/searchXinSortedArray.java
@@ -18,3 +18,23 @@ class Solution {
     return -1;
   }
 }
+
+class Solution {
+    public int binSearch(int nums[], int start, int end, int target){
+        while(start<=end){
+            int mid = start + (end - start) /2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target){
+               return binSearch(nums, start, mid -1, target);
+            } else {
+                return binSearch(nums, mid + 1, end, target);
+            }
+        }
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+       // recursive :
+       return binSearch(nums, 0, nums.length -1, target);
+    }
+}
