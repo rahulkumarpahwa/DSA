a0428568427496b1b815abc51b17a1744672b060
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-17 22:05:39 +0530

    Add optimal implementation for upper bound search using binary search

--- TUF+/BinarySearch/Fundamentals/upperBound.java
+++ TUF+/BinarySearch/Fundamentals/upperBound.java
@@ -10,3 +10,20 @@ class Solution {
     return nums.length;
   }
 }
+class Solution {
+    public int upperBound(int[] nums, int x) {
+        // optimal:
+        int ans = nums.length; 
+        int i = 0, j = nums.length -1;
+        while(i<=j){
+            int mid = i + (j - i) /2;
+            if(nums[mid] > x){
+                ans = mid;
+                j = mid -1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
