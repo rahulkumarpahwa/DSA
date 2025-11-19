c98bde3e2d459f6778f217edef2e5232340d43bc
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-19 23:27:39 +0530

    Refactor searchInsert method to implement optimal binary search algorithm

--- TUF+/BinarySearch/LogicBuilding/searchInsertPosition.java
+++ TUF+/BinarySearch/LogicBuilding/searchInsertPosition.java
@@ -13,3 +13,21 @@ class Solution {
     return nums.length; // dummy
   }
 }
+
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+    // optimal
+    int ans = nums.length;
+    int i = 0, j = nums.length - 1;
+    while (i <= j) {
+      int mid = i + (j - i) / 2;
+      if (nums[mid] >= target) {
+        ans = mid;
+        j = mid - 1; // going left to get the least value.
+      } else if (nums[mid] < target) {
+        i = mid + 1;
+      }
+    }
+    return ans;
+  }
+}
