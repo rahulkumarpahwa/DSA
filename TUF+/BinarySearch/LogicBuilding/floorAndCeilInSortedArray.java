1ec87dde9bd895388776ddf8ce6e88905eb6012e
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-21 22:03:35 +0530

    Add optimal implementation for floor and ceil in a sorted array using binary search

--- TUF+/BinarySearch/LogicBuilding/floorAndCeilInSortedArray.java
+++ TUF+/BinarySearch/LogicBuilding/floorAndCeilInSortedArray.java
@@ -18,3 +18,34 @@ class Solution {
     return new int[] {floor, ceil};
   }
 }
+
+
+class Solution {
+  public int[] getFloorAndCeil(int[] nums, int x) {
+    // optimal :
+    int floor = -1, ceil = -1;
+    int i = 0, j = nums.length - 1;
+    while (i <= j) {
+      int mid = i + (j - i) / 2;
+      if (nums[mid] >= x) {
+        ceil = nums[mid];
+        j = mid - 1;
+      } else {
+        i = mid + 1;
+      }
+    }
+
+    i = 0;
+    j = nums.length - 1;
+    while (i <= j) {
+      int mid = i + (j - i) / 2;
+      if (nums[mid] <= x) {
+        floor = nums[mid];
+        i = mid + 1;
+      } else {
+        j = mid - 1;
+      }
+    }
+    return new int[] {floor, ceil};
+  }
+}
