f488511ddafe140cbac066bb6f8a4c4bc274e28b
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-23 23:25:47 +0530

    Add optimal binary search implementation for searching in a rotated sorted array

--- TUF+/BinarySearch/LogicBuilding/searchInRotatedSortedArray_I.java
+++ TUF+/BinarySearch/LogicBuilding/searchInRotatedSortedArray_I.java
@@ -10,3 +10,29 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+  public int search(int[] nums, int k) {
+    // optimal:
+    int i = 0, j = nums.length - 1;
+    while (i <= j) {
+      int mid = i + (j - i) / 2;
+      if (nums[mid] == k) return mid;
+
+      if (nums[i] <= nums[mid]) {
+        if (nums[i] <= k && k <= nums[mid]) {
+          j = mid - 1;
+        } else {
+          i = mid + 1;
+        }
+      } else {
+        if (nums[mid] <= k && k <= nums[j]) {
+          i = mid + 1;
+        } else {
+          j = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
+}
