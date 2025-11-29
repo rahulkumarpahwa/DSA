396da9a1419a1892c26f27c248b9c5839c9340ff
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-11-29 22:52:19 +0530

    Add implementation for finding the single non-duplicate element using brute force with HashMap

---
+++ TUF+/Bit Manipulation/SingleNumber-I.java
@@ -0,0 +1,18 @@
+// https://takeuforward.org/plus/dsa/bit-manipulation/problems/single-number---i
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        // brute:
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
