4eca4d4df87cc147014fd73be70ffa206b7b34b2
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-12-02 21:47:40 +0530

    Add implementation for finding the single non-duplicate element in an array using sorting

---
+++ TUF+/Bit Manipulation/singleNumber-II.java
@@ -0,0 +1,20 @@
+// https://takeuforward.org/plus/dsa/bit-manipulation/problems/single-number---ii
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        //brute:
+        Arrays.sort(nums);
+        if(nums[0]!=nums[1]){
+          return nums[0];
+        }
+        if(nums[nums.length -2] != nums[nums.length -1]){
+          return nums[nums.length-1];
+        }
+        for(int i = 1; i<nums.length -1; i++){
+          if(nums[i-1] != nums[i] && nums[i] != nums[i+1]){
+            return nums[i];
+          }
+        }
+        return -1;
+    }
+}
