b2f465bcc6b3f4b6782244323f4959141d2ac353
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-12-03 22:17:12 +0530

    Add implementation for finding the single non-duplicate element using HashMap

--- TUF+/Bit Manipulation/singleNumber-II.java
+++ TUF+/Bit Manipulation/singleNumber-II.java
@@ -18,3 +18,21 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    // better 1:
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    }
+
+    for (int key : map.keySet()) {
+      if (map.get(key) == 1) {
+        return key;
+      }
+    }
+    return -1;
+  }
+}
